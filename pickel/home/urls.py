b5a4d9ee4e1c23904e6302048c42c1c56deb8e49
evanja57 <evanja57@gmail.com>
2024-01-14 01:41:42 -0800

    implimented nearest court finder

--- pickel/home/urls.py
+++ pickel/home/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-
 from . import views
 
 app_name = "home"
@@ -7,6 +6,7 @@ app_name = "home"
 urlpatterns = [
     path('home/', views.title, name='home'),
     path('queue/', views.queue, name='queue'),
+    path('search/', views.search, name='search'),
     path('beginner-guide/', views.beginner_guide, name='beginner_guide'),
     path('watch-pros/', views.watch_pros, name='watch_pros'),
 ]
--- pickel/home/views.py
+++ pickel/home/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.template import loader
 from django.http import Http404
-from django.shortcuts import reverse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, reverse
+import googlemaps
+from .forms import SearchForm
 
 def title(request):
     return render(request, "home/title.html", {})
@@ -21,12 +22,50 @@ def beginner_guide(request):
 def watch_pros(request):
     return render(request, "home/watch_pros.html")
 
+@csrf_exempt
+def search(request):
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            api_key = GOOGLE_API_KEY  # Replace with your actual Google Cloud Platform API key
+            search_location = form.cleaned_data['address']
+            
+            gmaps = googlemaps.Client(key=api_key)
+
+            geocoded = gmaps.geocode(search_location)[0]
+            
+            geometry = geocoded.get('geometry', {})
+            location = geometry.get('location', {})
+            lat = location.get('lat')
+            long = location.get('lng')
+
+            places_result = gmaps.places_nearby(
+                location=(lat, long),
+                radius=15000,
+                keyword='pickleball court'
+            )
+
+            if 'results' in places_result:
+                nearest_courts = [
+                    {
+                        'name': place['name'],
+                        'address': place.get('vicinity', 'N/A'),
+                        'location': place['geometry']['location']
+                    }
+                    for place in places_result['results']
+                ][:5]  # Take the first 5 results
+
+                return render(request, 'home/search.html', {'nearest_courts': nearest_courts, 'form': form})
+            else:
+                return JsonResponse({'error': 'No pickleball courts found nearby.'}, status=404)
+    else:
+        form = SearchForm()
+
+    return render(request, 'home/search.html', {'form': form})
 
 
 
 
 
 
-
-
-
+GOOGLE_API_KEY = "AIzaSyB9WLZBV4EICU-lXezlRvdLyhoPS3P0f6Y"
